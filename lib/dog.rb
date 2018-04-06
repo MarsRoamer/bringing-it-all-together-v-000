3c3c9dbf6309a0211b2244f02b2c048c9c7c4f26
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-06 15:22:01 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,66 @@
+require 'pry'
+
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(hash)
+    @id = hash[:id]
+    @name = hash[:name]
+    @breed = hash[:breed]
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+      DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+      DB[:conn].execute("DROP TABLE dogs")
+    end
+
+    def save
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        self
+    end
+
+    def self.create(hash)
+      dog = Dog.new(hash)
+      dog.save
+
+    end
+
+    def self.find_by_id(id)
+      dog = DB[:conn].execute("SELECT * FROM dogs WHERE id = id")[0]
+      hash = {}
+      hash[:id] = dog[0]
+      hash[:name] = dog[1]
+      hash[:breed] = dog[2]
+      Dog.new(hash)
+    end
+
+    def self.find_or_create_by(hash)
+      dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = hash[:name]")
+      binding.pry
+      
+
+    end
+    
+
+
+
+
+
+
+
+end
